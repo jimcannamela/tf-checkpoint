2ddaa88a7af91b602ce1e7788e091993c8dc428d
Jim Cannamela <jim.cannamela@outlook.com>
2023-09-06 15:15:34 -0500

    Changed Security Group to open port 8080 instead of 80 for http traffic

--- main.tf
+++ main.tf
@@ -51,8 +51,8 @@ resource "aws_security_group" "allowsshandhttp" {
 
   ingress {
     description = "Allow HTTP inbound traffic"
-    from_port   = 80
-    to_port     = 80
+    from_port   = 8080
+    to_port     = 8080
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
